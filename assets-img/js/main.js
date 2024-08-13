fd5b000a7726db446634f17c8f8e92eb34bdc1a5
Antonio Junior <antoniojrfsousa@gmail.com>
2024-08-12 21:47:44 -0300

    atualizações de arquivos index main e css portfolio

--- assets-img/js/main.js
+++ assets-img/js/main.js
@@ -1,17 +1,17 @@
 
-function updateProfileInfo(profileData){
+function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
-    
-    
+
+
 
     const name = document.getElementById('profile.nome')
     name.innerText = profileData.nome
-    
+
     const job = document.getElementById('profile.job')
-    job.innerText =profileData.job
-    
+    job.innerText = profileData.job
+
     const location = document.getElementById('profile.location')
     location.innerText = profileData.location
 
@@ -36,11 +36,49 @@ function updateHardSkills(profileData) {
 
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData) {
+    const profile = document.getElementById('profile.portifolio')
+
+    profile.innerHTML = profileData.portfolio.map(portfolio => {
+        return `
+        <li>
+            <h3 ${portfolio.git ? 'class="github"' : ''}>${portfolio.name}</h3>
+            <a href="${portfolio.url}" target="_blank">${portfolio.url}</a>
+        </li>`
+
+
+
+    }).join('')
+}
+
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return `
+        <li>    
+            <h3 class="title">${experience.name}</h3>
+            <p class="period">${experience.period}</p>
+            <p ${experience.description}</p>
+        </li>
+        `
+    }).join('')
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
     updateHardSkills(profileData)
-
+    updateProfessionalExperience(profileData)
 })()
